ef8e28bf3810460228126c8614ac10d4e82dc7b9
Дмитрий Квасов <dimonkwasoff@gmail.com>
2025-11-27 14:38:29 +0300

    Добавьте файлы проекта.

---
+++ FileStorageSystem/DataBase.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace FileStorageSystem
+{
+    public class DataBase
+    {
+        private string? _connectionString;
+        private SqlConnection? _connection;
+
+        public void DatabaseConnection(string connectionString)
+        {
+            _connectionString = connectionString;
+            _connection = new SqlConnection(_connectionString);
+        }
+
+        public SqlConnection GetConnection() => _connection;
+
+        public void OpenConnection()
+        {
+            try
+            {
+                if (_connection.State != System.Data.ConnectionState.Open)
+                {
+                    _connection.Open();
+                    Console.WriteLine("Подключение к базе данных успешно установлено.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка выполнения: {ex.Message}");
+                Console.WriteLine($"Внутреннее исключение: {ex.InnerException?.Message}");
+                Logger.LogError($"User=admin",
+                                $"DB connection error", ex);
+            }
+            Logger.LogInfo($"User=admin",
+                           $"Establishing a connection to the DB. " +
+                           $"id=0");
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+                if (_connection.State == System.Data.ConnectionState.Open)
+                {
+                    _connection.Close();
+                    Console.WriteLine("Подключение к базе данных закрыто.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка выполнения: {ex.Message}");
+                Console.WriteLine($"Внутреннее исключение: {ex.InnerException?.Message}");
+                Logger.LogError($"User=admin",
+                                $"DB closing error", ex);
+            }
+            Logger.LogInfo($"User=admin",
+                           $"Closing the connection to the DB. " +
+                           $"id=0");
+        }
+
+        // Пример выполнения запроса (можно добавить другие методы для разных операций)
+        public SqlDataReader ExecuteQuery(string query)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(query, _connection);
+                return command.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка выполнения: {ex.Message}");
+                Console.WriteLine($"Внутреннее исключение: {ex.InnerException?.Message}");
+                Logger.LogError($"User=admin",
+                                $"Error for executing a request - {query}", ex);
+            }
+            Logger.LogInfo($"User=admin",
+                           $"Executing a request {query}. " +
+                           $"id=0");
+            return null;
+        }
+
+        // Метод для выполнения запроса без возвращаемого значения (например, INSERT, UPDATE, DELETE)
+        public int ExecuteNonQuery(string query)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand(query, _connection);
+                return command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка выполнения: {ex.Message}");
+                Console.WriteLine($"Внутреннее исключение: {ex.InnerException?.Message}");
+                Logger.LogError($"User=admin",
+                                $"Error for executing a request - {query}", ex);
+            }
+            Logger.LogInfo($"User=admin",
+                           $"Executing a request {query}. " +
+                           $"id=0");
+            return 0;
+        }
+
+        // Для высвобождения ресурсов
+        public void Dispose()
+        {
+            try
+            {
+                if (_connection != null)
+                {
+                    if (_connection.State == System.Data.ConnectionState.Open)
+                    {
+                        _connection.Close();
+                    }
+                    _connection.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка выполнения: {ex.Message}");
+                Console.WriteLine($"Внутреннее исключение: {ex.InnerException?.Message}");
+                Logger.LogError($"User=admin",
+                                $"Error dispose id=0", ex);
+            }
+            Logger.LogInfo($"User=admin",
+                           $"Dispose to DB. " +
+                           $"id=0");
+        }
+    }
+}
---
+++ FileStorageSystem/Props.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FileStorageSystem
+{
+    public class Props
+    {
+        // Шифрование строки технологией SHA512
+        public static string ToSHA512(string TheEncryptionString)
+        {
+            using var sha512 = SHA512.Create();
+            byte[] bytes = sha512.ComputeHash(Encoding.UTF8.GetBytes(TheEncryptionString));
+
+            var userPasswordToHash = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                userPasswordToHash.Append(bytes[i].ToString("X2"));
+            }
+
+            Logger.LogInfo($"User=---Login_User---",
+                           $"Hashing user string {TheEncryptionString}. " +
+                           $"id=---ID_User---");
+
+            return userPasswordToHash.ToString();
+        }
+    }
+
+    // Логирование приложения
+    public static class Logger
+    {
+        private static readonly object _lock = new object();
+        private static string _logFilePath = "application_log.txt";
+
+        public static void LogInfo(string user, string action)
+            => WriteLog("INFO", user, action, null);
+        public static void LogError(string user, string action, Exception ex)
+            => WriteLog("ERROR", user, action, ex);
+
+        private static void WriteLog(string level, string user, string action, Exception? ex)
+        {
+            try
+            {
+                lock (_lock)
+                {
+                    using (var writer = new StreamWriter(_logFilePath, true))
+                    {
+                        string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                        writer.Write($"{time} [{level}] ");
+                        writer.Write($"User: {user}, Action: {action}");
+                        if (ex != null)
+                        {
+                            writer.Write($", Exception: {ex.GetType().Name}, Message: {ex.Message}, StackTrace: {ex.StackTrace}");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+            catch { }
+        }
+    }
+}
