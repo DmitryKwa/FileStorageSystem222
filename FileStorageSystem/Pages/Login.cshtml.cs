be4574975bbc367bb75b8771d87cb667387736e2
Дмитрий Квасов <dimonkwasoff@gmail.com>
2025-11-28 13:07:44 +0300

    123

--- FileStorageSystem/Pages/Login.cshtml.cs
+++ FileStorageSystem/Pages/Login.cshtml.cs
@@ -8,15 +8,5 @@ namespace FileStorageSystem.Pages
         public void OnGet()
         {
         }
-
-        public async Task<IActionResult> OnPostMyMethodAsync() // Новый метод для клика
-        {
-            // Ваша логика здесь (например, уведомление или работа с БД)
-            // Например, добавьте вывод в консоль или логику из предыдущего чата
-
-
-            // Верните RedirectToPage или PartialView, если нужно обновить страницу
-            return null;
-        }
     }
 }
